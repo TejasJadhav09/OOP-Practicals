8e6ab185fc7e22658cbf5283ea5aced126f70022
Tejas <149394295+TejasJadhav09@users.noreply.github.com>
2023-11-23 22:36:26 +0530

    Update Practical3.cpp

--- Practical3.cpp
+++ Practical3.cpp
@@ -1,59 +1,130 @@
 #include <iostream>
+#include <string>
 using namespace std;
-class publication 
-{
-public:
-string title;
-float price;
 
-void setdata(){
-cout<<"Enter title of book"<<endl;
-cin>>title;
- cout<<"Enter price of book"<<endl;
-cin>>price;
-}
+
+class publication{
+  string title;
+  float  price;
+  public:
+  
+  void get(){
+   cout<<"Enter Title of Book:"<<endl;
+   cin>>title;
+   cout<<"Enter Price of Book:"<<endl;
+   cin>>price;
+   }
+  void display(){
+  
+   cout<<" Title:"<<title;
+   cout<<" Price:"<<price;
+   }
+ };
  
-};
-class book:public publication
-{
-public:
-int pagecount;
+class book: public publication{
+ int page_count;
+ 
+ public:
 
-void setdata1(){
-cout<<"Enter number of pages of book"<<endl;
-cin>>pagecount;
-}
-void display1()
-{
-cout<<"title of book is"<<title<<endl;
-cout<<"price of book is"<<price<<endl;
-cout<<" number of pages of book is"<<pagecount<<endl;
-}
-};
-class tape:public publication
-{
-public:
-int playingtime;
+ 
+  
+ void add_book(){
+ publication::get();
+ 
+ 
+ 
+ 
+  
+   
+  
+  cout<<"Enter Page Count:"<<endl;
+  cin>>page_count;
+  if(page_count<=0){
+    cout<<"Page does Not Exist"<<endl;
+    }
+  
+  }
+ 
+  
+  
+  void display_book(){
+    publication::display();
+  
+  
+    
+    cout<<"Page Count:"<<page_count;
+   }
+  
+  
+  }; 
 
-void setdata2()
-{
-cout<<"Enter play time of book"<<endl;
-cin>>playingtime;
-}
-void display2()
-{
-cout<<" playing time of book is"<<endl;
-}
+class tape:public publication{
+ float playing_time;
+  
+ 
+ public:
+ 
+ 
+ 
+ void add_tape(){
+ publication::get();
+
+
+ 
+ 
+ 
+  cout<<"Enter Playing time:"<<endl;
+  cin>>playing_time;
+  if(playing_time<=0){
+  cout<<"playing time does not exist";
+  }
+  }
+ 
+  
+  void display_tape(){
+  publication::display();
+  
+  
+  
+  
+    cout<<"Playing time:"<<playing_time;
+   }
+    
 };
-int main()
-{
-book b;
-b.setdata();
-b.setdata1();
-b.display1();
-tape t;
-t.setdata2();
-t.display2();
+
+int main(){
+book obj1;
+tape obj2;
+int choice;
+while(1){
+cout<<"enter 1 for add book"<<endl;
+cout<<"enter 2 for display book"<<endl;
+cout<<"enter 3 for add tape"<<endl;
+cout<<"enter 4 for display tape"<<endl;
+cout<<" "<<endl;
+cout<<"Enter Your Choice:"<<endl;
+cin>>choice;
+
+switch(choice){
+  case 1:
+     
+     obj1.add_book();
+     break;
+  case 2:
+       
+     obj1.display_book();
+     break;
+  case 3:
+     
+     obj2.add_tape();
+     break;
+  case 4:
+     
+     obj2.display_tape();
+     break;  
+     
+    }
+   }          
 return 0;
 }
 
